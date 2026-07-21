2c2a5d5f308b9b8393162b2b89f8608c604b2511
dsx <25076778@qq.com>
2026-07-21 17:14:59 +0800

    选择用户组件调整以及新增后端角色权限控制

---
+++ api/permission/index.js
@@ -0,0 +1,21 @@
+﻿import createService from '/@/utils/service.js';
+
+/**
+ * 权限
+ */
+export function permissionApi() {
+    return createService(
+        {
+            list: {
+                name: '权限列表',
+                url: '/permission',
+                method: 'get',
+                params: {},
+                token: {
+                    name: 'token',
+                    value: true
+                }
+            },
+        }
+    );
+}
